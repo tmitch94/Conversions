041f6bdb7d74d1d368172f0faca9981608ff7636
Tacia Mitchell <Tacia. Mitchell@icloud.com>
2023-11-04 02:25:10 -0400

    created getters and setters

--- LengthConversion.java
+++ LengthConversion.java
@@ -1,3 +1,65 @@
 public class LengthConversion {
+private double inches;
+private double meters;
+private double yards;
+private double Km;
+private double Cm;
+private double miles;
 
+    public LengthConversion(double inches, double meters, double yards, double km, double cm, double miles) {
+        this.inches = inches;
+        this.meters = meters;
+        this.yards = yards;
+        Km = km;
+        Cm = cm;
+        this.miles = miles;
+    }
+
+    public double getInches() {
+        return inches;
+    }
+
+    public void setInches(double inches) {
+        this.inches = inches;
+    }
+
+    public double getMeters() {
+        return meters;
+    }
+
+    public void setMeters(double meters) {
+        this.meters = meters;
+    }
+
+    public double getYards() {
+        return yards;
+    }
+
+    public void setYards(double yards) {
+        this.yards = yards;
+    }
+
+    public double getKm() {
+        return Km;
+    }
+
+    public void setKm(double km) {
+        Km = km;
+    }
+
+    public double getCm() {
+        return Cm;
+    }
+
+    public void setCm(double cm) {
+        Cm = cm;
+    }
+
+    public double getMiles() {
+        return miles;
+    }
+
+    public void setMiles(double miles) {
+        this.miles = miles;
+    }
 }
