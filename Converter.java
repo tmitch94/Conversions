788413cfcede46b3aec8bab989fe99c5512b2b82
Tacia Mitchell <Tacia. Mitchell@icloud.com>
2023-11-15 02:45:29 -0500

    removed jframe to add layout manager

---
+++ Converter.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Converter  {
+    //create jframe with button
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
